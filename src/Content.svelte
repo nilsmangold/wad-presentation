63f0659f1cd85754baf2801b5e9e6ce9810929c4
Nils Mangold <nilsmangold@Air-von-Nils.fritz.box>
2021-07-29 22:08:03 +0200

    just build

--- src/Content.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  export let long;
-</script>
-
-{#if long}
-  <slot name="long" />
-{:else}
-  <slot name="short" />
-{/if}
--- src/examples/Counter.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  let count = 0;
-</script>
-
-<div>
-  <button on:click={() => count++}>
-    Ich wurde {count} mal geklickt!
-  </button>
-</div>
